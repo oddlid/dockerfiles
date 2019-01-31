5665bfdb07caf2f1dca11b0fa5282ce522b1e678
Odd E. Ebbesen <odd@oddware.net>
2019-01-31 23:34:38 +0100

    Updated for ZNC 1.7.2

--- znc/99-launch.sh
+++ znc/99-launch.sh
@@ -2,4 +2,4 @@
 # started via *shell module is not guaranteed to reap their children.
 # That's why using tini.
 
-exec /sbin/tini -g -- su-exec znc:znc /opt/znc/bin/znc --foreground --datadir "$DATADIR" "$@"
+exec su-exec znc:znc /opt/znc/bin/znc --foreground --datadir "$DATADIR" "$@"
